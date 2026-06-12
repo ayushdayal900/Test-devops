00eda20bd8ddd4ccd4a22065fe23bec9ec478b1e
Ayush Prashant Dayal <134316930+ayushdayal900@users.noreply.github.com>
2026-06-12 16:57:36 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ const allowedOrigins = [
     'http://localhost:5174',
     'http://localhost:5175',
     'http://localhost:5176',
+    'http://localhost:3000',
     'https://mahalaxmi-tailoring.vercel.app', // Vercel App
     'https://mahalaxmi-tailors.shop', // Custom Domain
     'https://www.mahalaxmi-tailors.shop', // Custom Domain (www)
